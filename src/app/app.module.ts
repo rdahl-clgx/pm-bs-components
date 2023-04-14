22cde16e52bc1ecc638537c89cd7a965cd37172d
Robbie Dahl <rdahl@corelogic.com>
2023-04-13 21:19:39 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { TooltipsComponent } from './components/tooltips/tooltips.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckboxComponent,
+    TooltipsComponent,
+    ModalComponent,
+    PaginationComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
