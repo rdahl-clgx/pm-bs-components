e7035232155480b239aee119da9344565cf0c4b2
David Brown <davidbrown@corelogic.com>
2023-04-20 11:34:25 -0700

    dev work completed and approved

--- src/app/components/tooltips/tooltips.component.ts
+++ src/app/components/tooltips/tooltips.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-tooltips',
   templateUrl: './tooltips.component.html',
-  styleUrls: ['./tooltips.component.scss']
+  styleUrls: ['./tooltips.component.scss'],
 })
-export class TooltipsComponent {
-	
-
+export class TooltipsComponent implements OnInit {
+  ngOnInit(): void {
+    // Bootstrap tooltip initialization
+    var tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }
 }
